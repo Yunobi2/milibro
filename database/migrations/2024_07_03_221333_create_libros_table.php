2ecbb9373ba058c30867160388512a735d50c5e8
Yunobi2 <yunobiluis@gmail.com>
2024-07-03 22:22:12 -0500

    actualizado

--- database/migrations/2024_07_03_221333_create_libros_table.php
+++ database/migrations/2024_07_03_221333_create_libros_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('titulo');
             $table->string('autor');
+            $table->integer('paginas');
             $table->year('ano');
             $table->string('editorial')->nullable();
             $table->string('categoria');
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,23 @@
-<nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <!-- Bootstrap CSS --> --}}
+    {{-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet"> 
+
+    <!-- Bootstrap JS and dependencies -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> 
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script> 
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">  --}}
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+
+</head>
+<body>
+    <nav class="main-nav">
         <img src="/milibro.png" alt="logo milibro" class=" w-24">
         <section class="nav-buttons">
             @auth
@@ -14,16 +33,21 @@
                 <img src="{{asset('icons/heart.svg')}}" alt="icono favoritos">
             </a>
             
-            <a class="logout"
-            class="dropdown-item" href="{{ route('logout') }}"
-            onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-            {{ __('Cerrar sesión') }}
-        </a>
-        
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-            @csrf
-        </form>
+            <div class="dropdown">
+                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-person-fill custom-icon"></i>
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                  <li>
+                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                      {{ __('Cerrar sesión') }}
+                    </a>
+                  </li>
+                </ul>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                  @csrf
+                </form>
+              </div>
         @else
         <a class="loginprueba"
         href="{{ route('login') }}"
@@ -34,3 +58,8 @@
         @endauth
     </section>
 </nav>
+</body>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</html>
